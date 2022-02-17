a5dc41f805c97905c1e4e83e32edf740d2bc4fde
Damilola Omifare <omifaredammy@gmail.com>
2022-02-17 13:04:18 +0000

    fix: testing

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>
+		..
 		<app-title>Contact Me</app-title>
 		<div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
 			<div class="relative bg-white shadow-xl border">
