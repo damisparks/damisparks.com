fd804d9bec00212217b459eac8643d1b689b9d16
Damilola Omifare <omifaredammy@gmail.com>
2023-04-18 14:21:47 +0100

    refactor: update component structure and metadata
    - update appherotext component
    - update nuxt config head metadata
    - update index with the new headline text

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,59 @@
 <script lang="ts" setup>
 const me = ref({
 	name: 'Dami Sparks',
-	role: ['Digital Freelancer', 'Full-Stack Software Engineer', 'Mentor'],
-	shortDescription: `I'm a digital freelance full-stack NodeJS engineer, mentor and technical writer passionate about crafting solid and scalable digital products and helping others find their passion for coding. I'm all about combining innovative development and product thinking to bring ideas into reality.`
+	role: ['Digital Freelancer', 'Full-Stack Developer', 'Mentor'],
+	shortDescription: `I'm a digital freelance full-stack developer, mentor and technical writer passionate about crafting solid and scalable digital products and helping others find their passion for coding. I'm all about combining innovative development and product thinking to bring ideas into reality.`,
+	want: 'Are you looking for a freelance Vue.js developer? 🔎',
+	wantCriteria: [
+		'Support to craft a complex web and mobile application to your needs?',
+		'A hybrid developer with frontend expertise with cross-discipline competencies in backend development, testing, continuous integration and development, and cloud computing?',
+		'A developer who focuses on clean, high-quality, scalable and well-documented code?',
+		'A developer who delivers products timely within the budget?'
+	]
 })
 </script>
 
 <template>
 	<NuxtLayout>
-		<div>
-			<AppHeroText>
-				<template #default>
-					<span class="mt-1">
-						<span
-							class="text-base font-normal text-ds-blue dark:text-ds-blue-accent-400"
-						>
-							Hi there, I am
-							<NuxtLink
-								:to="{ name: 'about' }"
-								class="font-montserrat underline"
-							>
-								{{ me.name }}
-							</NuxtLink>
-							✋🏾
-						</span>
-						<span
-							class="block my-2 font-montserrat text-zinc-500 dark:text-ds-smokewhite"
-						>
-							Are you looking for ?
-						</span>
-						<span
-							class="block text-ds-teal text-3xl tracking-tight font-bold sm:text-4xl xl:text-5xl space-y-3 font-montserrat"
-						>
-							<span v-for="(item, index) in me.role" :key="index">
-								<span v-if="index !== me.role.length - 1 && index !== 0">
-									,
-								</span>
-								<span v-if="index === me.role.length - 1" class="ml-2">&</span>
-								{{ item }}
-							</span>
-						</span>
-					</span>
-				</template>
-				<template #description>
-					<span
-						class="mt-3 text-base text-zinc-500 dark:text-ds-smokewhite sm:mt-5 sm:text-xl lg:text-lg xl:text-xl"
+		<div class="sm:py-24 sm:px-6 lg:px-8">
+			<header class="mt-8">
+				<div class="mx-auto max-w-2xl lg:max-w-5xl">
+					<h1
+						class="text-4xl font-normal tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl"
 					>
-						{{ me.shortDescription }}
-					</span>
-				</template>
-			</AppHeroText>
+						{{ me.want }}
+					</h1>
+				</div>
+			</header>
 
+			<section class="space-y-7 pt-8 lg:pt-16 mx-auto max-w-2xl lg:max-w-5xl">
+				<div>
+					<span
+						class="font-montserrat leading-tight text-3xl md:text-4xl lg:text-5xl text-zinc-600 dark:text-zinc-400 text-left"
+					>
+						Do you need:
+					</span>
+				</div>
+				<div>
+					<ul class="text-left space-y-7">
+						<li
+							v-for="(cr, idx) in me.wantCriteria"
+							:key="`wantCriteria-${idx}`"
+						>
+							<span
+								class="font-firasans text-base font-normal text-zinc-600 dark:text-zinc-400 tracking-tight"
+							>
+								<Icon
+									name="fluent:checkmark-circle-24-filled"
+									class="h-6 w-6 text-ds-teal"
+									aria-hidden="true"
+								/>
+								{{ cr }}
+							</span>
+						</li>
+					</ul>
+				</div>
+			</section>
 			<div class="mt-8 sm:max-w-lg sm:mx-auto text-center">
 				<NuxtLink :to="{ name: 'about' }" class="btn btn-ds-blue">
 					Learn More
