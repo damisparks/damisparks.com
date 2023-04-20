7a34e43d876c9095e43ded02d83f3f883117c4e3
Damilola Omifare <omifaredammy@gmail.com>
2023-04-20 16:52:54 +0100

    feat: add project spotlight accordion template

---
+++ components/ProjectSpotlightAccordion.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+</script>
+
+<template>
+	<div
+		class="mx-auto w-full max-w-md rounded-lg bg-zinc-100 dark:bg-zinc-700 shadow-sm"
+	>
+		<Disclosure v-slot="{ open }">
+			<DisclosureButton
+				class="flex w-full justify-between rounded-lg px-4 py-4 text-left font-medium text-lg text-zinc-800 dark:text-zinc-100 hover:text-zinc-900 focus:outline-none focus-visible:ring focus-visible:ring-zinc-500 focus-visible:ring-opacity-75"
+			>
+				<slot name="header">Header</slot>
+				<Icon
+					name="fluent:chevron-circle-up-16-regular"
+					:class="open ? 'rotate-180 transform' : ''"
+					class="h-8 w-8"
+				/>
+			</DisclosureButton>
+			<DisclosurePanel
+				class="px-4 pb-4 text-sm text-zinc-500 dark:text-ds-smokewhite"
+			>
+				<hr class="mb-4 h-px border-0 bg-zinc-400 dark:bg-zinc-200" />
+
+				<slot name="content">Content go here</slot>
+			</DisclosurePanel>
+		</Disclosure>
+	</div>
+</template>
