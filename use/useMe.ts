202db5fde3758b6694f566847f4a09b4055ed27e
Damilola Omifare <omifaredammy@gmail.com>
2023-04-21 14:15:25 +0100

    feat: add capitalizeWords function
    Add TypeScript function to capitalize the first letter of each word in a string.

--- use/useMe.ts
+++ use/useMe.ts
@@ -15,12 +15,34 @@ export const capitalizeText = (text = ''): string => {
 }
 
 /**
+ * Capitalizes the first letter of each word in a string.
+ * @param str - The input string.
+ * @returns The input string with the first letter of each word capitalized.
+ * @throws {TypeError} If the input is not a string.
+ */
+const capitalizeWords = (str: string): string => {
+	if (typeof str !== 'string') {
+		throw new TypeError('Input must be a string.')
+	}
+
+	return str
+		.split(' ')
+		.map(word => {
+			const firstLetter = word.charAt(0).toUpperCase()
+			const restOfString = word.slice(1)
+			return `${firstLetter}${restOfString}`
+		})
+		.join(' ')
+}
+
+/**
  * Returns an object with utility functions for working with text.
  * @returns An object with a `capitalizeText` function.
  */
 
 export function useMe() {
 	return {
-		capitalizeText
+		capitalizeText,
+		capitalizeWords
 	}
 }
