4b665cfc4eeeaa41e03fa41200e26d1841ab8c62
Damilola Omifare <omifaredammy@gmail.com>
2023-04-27 21:03:47 +0100

    refactor: about page keywords

--- pages/about.vue
+++ pages/about.vue
@@ -3,6 +3,17 @@ import { awards as achievements } from '@/utils'
 </script>
 <template>
 	<NuxtLayout>
+		<Head>
+			<Title>About</Title>
+			<Meta
+				name="description"
+				content="Learn more about Dami Sparks - Freelance Full-Stack Developer, Mentor, Technical Writer and Fractional CTO"
+			/>
+			<Meta
+				name="keywords"
+				content="Dami Sparks, About Dami Sparks, Full-Stack Developer, Technical Writer, Mentor, Fractional CTO, Innovative Development, Product Thinking, Scalable Digital Products, RESTful API Integration, Cloud Platform Deployment, Clean Code, High-Quality Code, Well-Documented Code, Client Satisfaction, Industry Expertise, Agile Development, Test-Driven Development, DevOps, Front-end Development, Back-end Development, Database Development, Web Development, Mobile Development, Custom Web Applications, E-commerce Development, CMS Development, UI/UX Design, Cross-Platform Development, Progressive Web Applications, Single Page Applications, Server-Side Rendering, Technical Consulting, Project Management, Startup Development, MVP Development, API Design, Technical SEO, Content Creation, Technical Marketing, Growth Hacking, Digital Strategy, Branding, Product Design, User Research, User Testing, Accessibility"
+			/>
+		</Head>
 		<AppPageHeader>
 			<AppTitle>Meet Dami</AppTitle>
 		</AppPageHeader>
