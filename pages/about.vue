0720d378f548296ca4e254917b4711904957bf4c
Damilola Omifare <omifaredammy@gmail.com>
2023-04-27 21:58:46 +0100

    refactor: update about keyword and metadata

--- pages/about.vue
+++ pages/about.vue
@@ -13,6 +13,7 @@ import { awards as achievements } from '@/utils'
 				name="keywords"
 				content="Dami Sparks, About Dami Sparks, Full-Stack Developer, Technical Writer, Mentor, Fractional CTO, Innovative Development, Product Thinking, Scalable Digital Products, RESTful API Integration, Cloud Platform Deployment, Clean Code, High-Quality Code, Well-Documented Code, Client Satisfaction, Industry Expertise, Agile Development, Test-Driven Development, DevOps, Front-end Development, Back-end Development, Database Development, Web Development, Mobile Development, Custom Web Applications, E-commerce Development, CMS Development, UI/UX Design, Cross-Platform Development, Progressive Web Applications, Single Page Applications, Server-Side Rendering, Technical Consulting, Project Management, Startup Development, MVP Development, API Design, Technical SEO, Content Creation, Technical Marketing, Growth Hacking, Digital Strategy, Branding, Product Design, User Research, User Testing, Accessibility"
 			/>
+			<Link rel="canonical" href="https://damisparks.com/about" />
 		</Head>
 		<AppPageHeader>
 			<AppTitle>Meet Dami</AppTitle>
