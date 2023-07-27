291bf2af6f1057b20eb7a80ed3a5f90076875e27
Damilola Omifare <omifaredammy@gmail.com>
2023-07-27 14:16:09 +0100

    feat: add blog setup

---
+++ layouts/blog.vue
@@ -0,0 +1,12 @@
+<template>
+	<main class="h-screen flex flex-col">
+		<AppNavbar />
+		<!-- main -->
+		<div class="flex-auto mt-6">
+			<AppContainer><slot /></AppContainer>
+		</div>
+		<div>
+			<AppFooter />
+		</div>
+	</main>
+</template>
---
+++ pages/blog/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+<template>
+	<NuxtLayout name="blog">
+		<header class="relative">
+			<div class="absolute inset-0 flex items-center" aria-hidden="true">
+				<div class="w-full border-t border-zinc-300" />
+			</div>
+			<div class="relative flex justify-center">
+				<h1
+					class="app-heading bg-zinc-100 dark:text-zinc-300 dark:bg-black px-3 font-light text-xl md:text-2xl"
+				>
+					Articles
+				</h1>
+			</div>
+		</header>
+	</NuxtLayout>
+</template>
+
+<style scoped></style>
