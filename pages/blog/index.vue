49304f70db283a4f8f0ddfe082ec8aadd908e2d1
Damilola Omifare <omifaredammy@gmail.com>
2023-07-28 12:22:11 +0100

    feat: add intro to blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 definePageMeta({ title: 'Blog' })
+
+const { data: entries } = useAsyncData('blogentries', () => {
+	return queryContent('/blog').find()
+})
 </script>
 <template>
 	<NuxtLayout name="blog">
@@ -15,5 +19,20 @@ definePageMeta({ title: 'Blog' })
 				</h1>
 			</div>
 		</header>
+		<!-- <pre>{{ entries }}</pre> -->
+		<div
+			class="grid grid-cols-1 space-y-6 sm:space-y-0 sm:grid-cols-2 sm:gap-6 py-4"
+		>
+			<article v-for="{ path, title, date } in entries" :key="path">
+				<div
+					class="shadow-sm p-4 rounded bg-zinc-200/90 text-zinc-700 dark:text-zinc-300 dark:bg-zinc-700/90"
+				>
+					<span class="text-sm">
+						<time :datetime="date" class="text-zinc-500">{{ date }}</time>
+					</span>
+					<p>{{ title }}</p>
+				</div>
+			</article>
+		</div>
 	</NuxtLayout>
 </template>
