ef851662f3f6c50fbc55948016a71b1008fbfecd
Damilola Omifare <omifaredammy@gmail.com>
2023-08-02 23:15:06 +0100

    chore: update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	ssr: false,
-	nitro: {
-		preset: 'service-worker',
-	},
-
 	app: {
 		head: {
 			htmlAttrs: { lang: 'en' },
