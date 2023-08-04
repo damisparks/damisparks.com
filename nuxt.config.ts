3f36cbe7e3edebdc9c18376b990ca345e1748c93
Damilola Omifare <omifaredammy@gmail.com>
2023-08-04 15:10:01 +0100

    refactor: update content, logics to use markdown

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -70,7 +70,7 @@ export default defineNuxtConfig({
 					href: 'https://fonts.gstatic.com',
 				},
 				{
-					href: 'https://fonts.googleapis.com/css2?Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Fira+Sans:ital,wght@0,100;0,400;0,600;0,700;1,400&display=swap',
+					href: 'https://fonts.googleapis.com/css2?Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Fira+Sans:ital,wght@0,100;0,400;0,500;0,600;0,700;1,400&display=swap',
 					rel: 'stylesheet',
 				},
 			],
--- pages/index.vue
+++ pages/index.vue
@@ -1,94 +1,16 @@
 <script lang="ts" setup>
 import { projectSpotlight as spotlights } from '@/constants'
-const me = ref({
-	name: 'Dami',
-	shortDescription: `I'm a digital freelance full-stack developer, mentor and technical writer passionate about crafting solid and scalable digital products and helping others find their passion for coding. I'm all about combining innovative development and product thinking to bring ideas into reality.`,
-	role: 'Vue.js Fullstack Software Developer | Mentor | Digital Freelancer',
-	wantCriteria: [
-		'A skilled Vue.js developer to build responsive and user-friendly interfaces for your web applications',
-		'Expertise in integrating Vue.js with RESTful APIs and deploying on cloud platforms like AWS, Azure and Google Cloud',
-		'A reliable and experienced freelance developer who can deliver scalable applications timely within the budget for clients across various industries',
-		'A developer who focuses on clean, high-quality and well-documented code',
-	],
-	specialisation: `I specialise in frontend and backend development, testing, data science, and cloud computing. So let's work together to bring your project to life.`,
-})
 </script>
 
 <template>
 	<NuxtLayout>
-		<div class="sm:py-24 sm:px-6 lg:px-8">
-			<header class="leading-none mt-[5vw] mb-[1vw]">
-				<h1
-					class="text-2xl font-normal tracking-tight text-zinc-800 dark:text-zinc-100"
-				>
-					Welcome!
-				</h1>
+		<div class="py-4 sm:py-8 lg:px-8">
+			<header class="leading-none mb-[1vw]">
+				<AppTitle>Dami Sparks Omifare</AppTitle>
 			</header>
 
-			<StaticMarkdownRenderer path="/" />
-
-			<section class="space-y-7 pt-8 lg:pt-16 mx-auto max-w-2xl lg:max-w-5xl">
-				<div>
-					<span
-						class="font-montserrat leading-tight text-3xl md:text-4xl lg:text-5xl text-zinc-600 dark:text-zinc-400 text-left"
-					>
-						Do you need:
-					</span>
-				</div>
-				<div>
-					<ul class="text-left space-y-7">
-						<li
-							v-for="(cr, idx) in me.wantCriteria"
-							:key="`wantcriteria-${idx}`"
-						>
-							<span
-								class="font-firasans text-base font-normal text-zinc-600 dark:text-zinc-400 tracking-tight"
-							>
-								<Icon
-									name="fluent:checkmark-circle-24-filled"
-									class="h-6 w-6 text-ds-teal"
-									aria-hidden="true"
-								/>
-								{{ cr }}
-							</span>
-						</li>
-					</ul>
-				</div>
-			</section>
-			<section>
-				<hr class="my-10 h-px border-0 bg-zinc-200 dark:bg-zinc-300" />
-
-				<div class="flex flex-col gap-10 sm:flex-row">
-					<div class="max-w-xl flex-auto">
-						<h3
-							class="text-lg font-firasans font-semibold leading-8 tracking-tight text-zinc-800 dark:text-zinc-100"
-						>
-							<NuxtLink :to="{ name: 'about' }" class="underline">
-								I am {{ me.name }}
-							</NuxtLink>
-						</h3>
-						<p class="text-base leading-7 text-zinc-600 dark:text-zinc-400">
-							{{ me.role }}
-						</p>
-						<p
-							class="mt-6 text-base leading-7 text-zinc-600 dark:text-zinc-400"
-						>
-							{{ me.specialisation }}
-						</p>
-						<div role="list" class="mt-6 flex gap-x-6">
-							<AppSocials />
-						</div>
-					</div>
-				</div>
-				<div class="mt-16 sm:max-w-xl sm:mx-auto text-center">
-					<NuxtLink
-						:to="{ name: 'contact' }"
-						class="btn btn-ds-blue font-firasans font-semibold w-full block"
-					>
-						<Icon name="fa6-solid:laptop-code" class="h-6 w-5 mr-3" />
-						<span>Hire me</span>
-					</NuxtLink>
-				</div>
+			<section class="text-zinc-600 dark:text-zinc-300">
+				<StaticMarkdownRenderer path="/" />
 			</section>
 
 			<hr class="my-10 h-px border-0 bg-zinc-200 dark:bg-zinc-300" />
