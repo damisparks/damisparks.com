12721e2c678c329f7544e4fe557d014b1426a98c
Damilola Omifare <omifaredammy@gmail.com>
2023-08-04 15:10:01 +0100

    refactor: update content, logics to use markdown

---
+++ components/content/ProseA.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+	href: {
+		type: String,
+		default: '',
+	},
+	target: {
+		type: String,
+		default: undefined,
+		required: false,
+	},
+})
+</script>
+<template>
+	<NuxtLink
+		:href="href"
+		:target="target"
+		class="hover:underline text-zinc-950 font-firasans font-medium dark:text-white"
+	>
+		<slot />
+	</NuxtLink>
+</template>
