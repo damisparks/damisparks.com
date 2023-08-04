cea80df2eab590d148d73e85e6872dcbd5fc78d1
Damilola Omifare <omifaredammy@gmail.com>
2023-08-04 20:58:32 +0100

    fix: udpate about content

--- content/about.md
+++ content/about.md
@@ -1,23 +1,22 @@
 ---
 title: About
-description: Fractional CTO | Passionate Open Source Enthusiast | Vue.js & Nuxt Fullstack Developer | Mentor | Technical Writer
+description: Dami is a Fractional CTO | Passionate Open Source Enthusiast | Vue.js & Nuxt Fullstack Developer | Mentor | Technical Writer
 ---
 
-He founded Actif, an Age-tech SaaS startup and continues contributing to the European developer community.
+_Hi there, Welcome! you made it._
 
-He is a passionate open-source enthusiast and a Vue.js & Nuxt full-stack developer. He is also a mentor and technical writer.
+He founded [Actif](https://www.actif.online/), an Age-tech SaaS startup and continues contributing to the European developer community.
 
 He aims to craft solid and scalable digital products while helping others find their passion for coding. He is all about combining innovative development and critical thinking to make ideas a reality.
 
-His passion lies in curiosity by envisioning ideas and turning them into reality. You can find his [complete projects list here](/projects).
-
 He is deeply involved in the vibrant open-source community by contributing to [Nuxt-validate](https://github.com/lewyuburi/nuxt-validate), [@nuxtjs/eslint-module](https://github.com/nuxt-modules/eslint), [@nuxtjs/google-fonts](https://github.com/nuxt-modules/google-fonts) and more.
+
+His passion lies in curiosity by envisioning ideas and turning them into reality. You can find my [complete projects list here](/projects).
 
 These projects span various domains, from input validation and full-stack applications to Azure AI certification resources and Nuxt.js modules. I actively contribute to these projects and continue to maintain and improve them.
 
-Let's Collaborate:
-He is your person if you're looking for a skilled Vue.js developer to build responsive and user-friendly interfaces for your web applications. My focus on clean, high-quality, well-documented code ensures I deliver scalable applications on time and within budget.
+He is your person if you're looking for a Vue.js developer to build responsive and user-friendly interfaces for your web applications. My focus on clean, high-quality, well-documented code ensures I deliver scalable applications on time and within budget.
 
 He is available for freelance projects, mentorship opportunities, and technical writing engagements. Let's work together to bring your project to life and make a lasting impact.
 
-When I'm not coding, I indulge in music. I play the drum 🥁 and guitar 🎸, fueling my creativity and inspiring my development journey. I also enjoy photography and thrills, seeking inspiration from the world around me.
+When I'm not coding, I indulge in music. I play the drum 🥁 and guitar 🎸, fueling my creativity and inspiring my development journey. I also enjoy playing tennis and dancing, seeking inspiration from the world around me.
--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts">
-import { awards as achievements } from '@/constants'
-</script>
 <template>
 	<NuxtLayout>
 		<Head>
@@ -15,86 +12,12 @@ import { awards as achievements } from '@/constants'
 			/>
 		</Head>
 		<AppPageHeader>
-			<AppTitle>Meet Dami</AppTitle>
+			<header>
+				<AppTitle>Meet Dami</AppTitle>
+			</header>
 		</AppPageHeader>
-		<div class="relative mt-16">
-			<div class="mx-auto max-w-2xl lg:max-w-5xl font-firasans">
-				<div
-					class="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12"
-				>
-					<div class="lg:pl-20">
-						<div class="max-w-xs px-2.5 lg:max-w-none">
-							<NuxtImg
-								loading="lazy"
-								class="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-								provider="cloudinary"
-								src="/damisparks.com/ds-portrait-tiny.webp"
-								height="800"
-								width="800"
-								format="webp"
-								alt="dami sparks omifare"
-							/>
-						</div>
-					</div>
-					<div class="lg:order-first lg:row-span-2">
-						<h1
-							class="text-4xl font-medium tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl"
-						>
-							Dami Sparks
-						</h1>
-
-						<div
-							class="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400"
-						>
-							<p>
-								Dami is the CTO and co-founder of Actif, a tech company focusing
-								on improving the health of older people with fitness and
-								cognitive programs.
-							</p>
-							<p>
-								He is a Certified Mentor, Machine Learning Engineer and Cloud
-								Developer with experience in front-end and back-end design and
-								development. Over the last five years, He has gained expertise
-								in designing and developing web applications, writing technical
-								documentation, and mentoring others working for tech companies
-								like Udacity, Photoweb, PrimeIT, Fujitsu.
-							</p>
-							<p>
-								He hosted the Ask Me Anything Session organised by Udacity
-								Introduction to Programming Nanodegree. His Team won the Demium
-								AllStartup Weekend 2020. Dami loves what he does, and he
-								inspires others to do what they love and become their heroes.
-							</p>
-							<NuxtLink
-								class="btn btn-ds-blue inline-flex"
-								to="https://drive.google.com/uc?export=download&id=1qRSxju1bsJWOt098a9EXPpx8rOsgL-8m"
-							>
-								View Resume
-							</NuxtLink>
-						</div>
-					</div>
-				</div>
-
-				<hr class="my-10 h-px border-0 bg-zinc-200 dark:bg-zinc-300" />
-
-				<!-- achievement & milestones -->
-				<div>
-					<h2
-						class="uppercase text-xl sm:text-2xl md:text-3xl tracking-widest font-light text-zinc-800 dark:text-ds-orange my-6"
-					>
-						achievements
-					</h2>
-
-					<div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-y-3 sm:gap-6">
-						<!-- achievement card -->
-						<AppAchievementCard
-							v-for="achievement in achievements"
-							:key="`achievements-${achievement.id}`"
-							:achievement="achievement"
-						/>
-					</div>
-				</div>
-			</div>
-		</div>
+		<main>
+			<ContentDoc path="about" />
+		</main>
 	</NuxtLayout>
 </template>
