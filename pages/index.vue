e92cf6d350aabbfc5721890dbbde44400893b267
Damilola Omifare <omifaredammy@gmail.com>
2023-08-04 21:29:46 +0100

    style: update page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
 	<NuxtLayout>
-		<header class="leading-none mb-[1vw]">
-			<AppTitle>Dami Sparks Omifare</AppTitle>
-		</header>
+		<AppPageHeader>
+			<AppTitle>Dami Sparks</AppTitle>
+		</AppPageHeader>
 		<main class="text-zinc-600 dark:text-zinc-300">
-			<hr class="my-4 h-px border-0 bg-zinc-200 dark:bg-zinc-300" />
-			<ContentDoc path="/" />
+			<div class="max-w-7xl mx-auto">
+				<section class="max-w-2xl mx-auto">
+					<hr class="my-4 h-px border-0 bg-zinc-200 dark:bg-zinc-300" />
+					<ContentDoc path="/" />
+				</section>
+			</div>
 		</main>
 	</NuxtLayout>
 </template>
