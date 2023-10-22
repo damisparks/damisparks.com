167735427c499e4a8386bbeae96976a9a77a16cb
Dami Sparks <damisparks@outlook.com>
2023-10-22 14:58:31 +0200

    chore: refactor eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,9 @@
-module.exports = {
+'use strict'
+
+const eslintConfig = {
 	root: true,
 	extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
 	// plugin: ['prettier']
 }
+
+module.exports = eslintConfig
