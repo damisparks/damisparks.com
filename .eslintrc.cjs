23bfeac86c754843ba8c20f432ebe33663e9c424
Dami Sparks <damisparks@outlook.com>
2023-10-23 13:13:53 +0200

    chore: add comment to eslintrc for eslint-mdx issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,8 @@
 const eslintConfig = {
 	root: true,
 	extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
+
+	/** @see https://github.com/mdx-js/eslint-mdx/issues/251#issuecomment-736417801 */
 	overrides: [
 		{
 			files: ['*.mdx', '*.md'],
