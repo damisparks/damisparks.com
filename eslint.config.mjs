e59bc29191c33b1ec1c047cfd7b0ddf51e3084d4
Dami Sparks <damisparks@outlook.com>
2024-04-14 22:30:50 +0200

    chore: update package and confg

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt([
+	{
+		rules: {
+			'@stylistic/space-before-function-paren': ['error', 'always'],
+			'@stylistic/arrow-parens': ['error', 'as-needed'],
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/ban-types': 'off',
+			// 'vue/block-order': ['error', { order: ['script', 'template', 'style'] }],
+		},
+	},
+	{
+		files: ['*.config.js'],
+		rules: {
+			'no-undef': 'off',
+		},
+	},
+])
