17daf7047e581eaeec75f7a6debe6932c375bda5
Dami Sparks <damisparks@outlook.com>
2024-04-25 20:36:18 +0200

    style: update styles

--- src/ui.config/nav.ts
+++ src/ui.config/nav.ts
@@ -1,13 +1,13 @@
 export default {
 	base: 'flex items-center justify-between',
 	paddingX: 'px-4 md:px-6 lg:px-8 mx-auto',
-	position: 'fixed top-0 left-0 w-full z-50',
+	position: 'fixed w-full z-50',
 	height: 'h-11 max-h-11',
 	background: 'bg-gray-200/50 dark:bg-gray-800/50',
 	color: 'text-gray-700 dark:text-gray-300',
 	backdrop: 'backdrop-saturate-100 backdrop-blur-lg',
 	inner: {
-		base: 'flex items-center justify-end space-x-6 w-full',
+		base: 'flex items-center justify-end space-x-6',
 		font: 'font-medium',
 		textSize: 'text-sm',
 	},
