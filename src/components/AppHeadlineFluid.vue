fdab8879cb491cb3873beae40ce181686de99c69
Dami Sparks <damisparks@outlook.com>
2024-05-01 00:06:42 +0200

    feat: add headline

---
+++ src/components/AppHeadlineFluid.vue
@@ -0,0 +1,7 @@
+<template>
+	<h3
+		class="font-montserrat text-[5vw] sm:text-[3vw] font-normal tracking-[-.5px] leading-[1.2em] lg:text-[28px] lg:leading-[36px]"
+	>
+		<slot />
+	</h3>
+</template>
