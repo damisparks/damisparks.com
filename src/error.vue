d6c5b5e7d89f0ed0a63d52c51fd9adc84cb8780a
Dami Sparks <damisparks@outlook.com>
2024-05-05 14:44:20 +0200

    chore: update nuxt and error page

--- src/error.vue
+++ src/error.vue
@@ -7,13 +7,11 @@ const handleError = () => clearError({ redirect: '/' })
 <template>
 	<main class="isolate relative min-h-screen">
 		<figure>
-			<NuxtPicture
-				src="/unsplash/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
+			<NuxtImg
+				src="/images/photo-1545972154-9bb223aac798.webp"
 				format="webp"
-				width="3750"
-				height="2500"
+				loading="lazy"
 				class="absolute inset-0 -z-10 h-full w-full object-cover object-top"
-				sizes="xs:200px md:500px lg:1024"
 			/>
 		</figure>
 		<div class="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
