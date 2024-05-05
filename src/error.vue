a6a456ddaa5ad67e65014795084352f67383105c
Dami Sparks <damisparks@outlook.com>
2024-05-05 13:59:17 +0200

    chore: update nuxt config

--- src/error.vue
+++ src/error.vue
@@ -8,7 +8,6 @@ const handleError = () => clearError({ redirect: '/' })
 	<main class="isolate relative min-h-screen">
 		<NuxtImg
 			src="/unsplash/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
-			format="webp"
 			class="absolute inset-0 -z-10 h-full w-full object-cover object-top"
 		/>
 		<div class="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
