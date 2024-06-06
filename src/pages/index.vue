76649a60540e12e53826e75f1dfa0d0d0cd0eec2
Dami Sparks <damisparks@outlook.com>
2024-06-06 23:11:22 +0200

    chore: update alt

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -64,7 +64,7 @@ const moreShowcase = showcase.slice(3)
               <NuxtImg
                 class="object-cover object-center col-span-2 w-full h-16 rounded-md"
                 :src="item.imageUrl"
-                :alt="`Image of ${item.name} product showcase`"
+                :alt="`image of ${item.name} product showcase graphic`"
               />
               <div class="col-span-4">
                 {{ item.name }}
