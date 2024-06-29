797bc115840dd5e2244a8eaac36131937d1b3164
Dami Sparks <damisparks@outlook.com>
2024-06-29 14:08:46 +0200

    fix(deps): update non-major dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4
+  },
   devtools: { enabled: true },
   // https://content.nuxtjs.org/guide/recipes/sitemap
   nitro: {
