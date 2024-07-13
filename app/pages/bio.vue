db88d121ee82b7a4bd63e5ccbcfbb3a3e34a70f5
Dami Sparks <damisparks@outlook.com>
2024-07-13 13:24:56 +0200

    update bio page

--- app/pages/bio.vue
+++ app/pages/bio.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-definePageMeta({ title: 'Bio', description: 'A little about me.' })
+const title = 'Bio'
+const description = 'A little about me.'
+useSeoMeta({
+  title,
+  description,
+  ogDescription: description,
+  ogTitle: title
+})
+
+const { data: bio } = await useAsyncData(
+  'bio',
+  () =>
+    ((import.meta.server || import.meta.dev) as true)
+    && queryContent('bio').findOne()
+)
+
+if (!bio.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Bio not found', fatal: true })
+}
 </script>
 
 <template>
-  <div class="py-8">
-    <ContentDoc path="bio" />
-  </div>
+  <NoteIsland>
+    <ContentRenderer :value="bio!" />
+  </NoteIsland>
 </template>
