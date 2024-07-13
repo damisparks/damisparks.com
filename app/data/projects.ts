05a64d23b2e6f74d13bba3e4940258d12f7a3a06
Dami Sparks <damisparks@outlook.com>
2024-07-13 15:06:03 +0200

    update page and add images

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,92 +1,78 @@
-type ProjectListTypes = Record<string, any[]>
+type ProjectItemTypes = {
+  id: string
+  name: string
+  description: string
+  websiteUrl: string
+  imageUrl?: string
+}
 
-const showcase = [
+const showcase: ProjectItemTypes[] = [
   {
     id: 'actifage-lda-actif-for-professionals',
     name: 'Actif for Professionals',
     description: 'Elder Care Solution with Adaptive Exercises and Facilities Management',
     websiteUrl: 'https://pro.actif.online/login',
-    imageUrl: '/images/actif-pro.webp'
+    imageUrl: '/projects/actif-for-pro.png'
   },
   {
     id: 'actifage-lda-actif-for-individuals',
     name: 'Actif for Individuals',
     description: 'Elder Care Solution with Adaptive Exercises for Individuals.',
     websiteUrl: 'https://me.actif.online/login',
-    imageUrl: '/images/actif-individual.webp'
+    imageUrl: '/projects/actif-for-individuals.png'
   },
   {
+    id: 'photoweb-izzy',
     name: 'Photoweb | Izzy',
     websiteUrl: 'http://photoweb.fr/',
     description: 'Photoweb is a French company that offers a wide range of photo products.',
-    imageUrl: '/images/photoweb.webp'
+    imageUrl: '/projects/photoweb-izzy.png'
   },
   {
     id: 'actifage-lda-actif-online',
     name: 'Actif Online',
     websiteUrl: 'https://www.actif.online/',
-    description: 'A tech company focusing on empowering older adults through exercise, anytime and everywhere.',
-    imageUrl: '/images/actif-main.webp'
+    description: 'Age-tech startup focusing on empowering older adults through exercise.',
+    imageUrl: '/projects/actif-homepage.png'
   }
 ]
-const all: ProjectListTypes = {
-  'business': [
-    {
-      id: 'actifage-lda-actif-online',
-      name: 'Actif Online',
-      websiteUrl: 'https://www.actif.online/',
-      description: 'A tech company focusing on empowering older adults through exercise, anytime and everywhere.'
-    },
-    {
-      id: 'actifage-lda-actif-for-professionals',
-      name: 'Actif for Professionals',
-      description: 'Elder Care Solution with Adaptive Exercises and Facilities Management',
-      websiteUrl: 'https://pro.actif.online/login'
-    },
-    {
-      id: 'actifage-lda-actif-for-individuals',
-      name: 'Actif for Individuals',
-      description: 'Elder Care Solution with Adaptive Exercises for Individuals.',
-      websiteUrl: 'https://me.actif.online/login'
-    },
-    {
-      name: 'Photoweb | Izzy',
-      websiteUrl: 'http://photoweb.fr/',
-      description: 'Photoweb is a French company that offers a wide range of photo products.'
-    }
-  ],
-  'open-source': [
-    {
-      id: 'how-to-become-microsoft-certified-azure-ai-engineer-associate',
-      name: 'How to become Microsoft Certified: Azure AI Engineer Associate',
-      websiteUrl: 'https://github.com/damisparks/microsoft-azure-ai-engineer-associate',
-      description: `Here are notes and resources I used for those pursuing certification in Cloud Solution Architecture, Azure AI, and development.`
-    },
-    {
-      id: 'nuxt-validate',
-      name: 'Nuxt-validate',
-      websiteUrl: 'https://www.npmjs.com/package/nuxt-validate',
-      description: 'A Simple Nuxt input validation module using vee-validate library. I am actively contributing to this project.'
-    },
-    {
-      id: 'nuxt-js-eslint-module',
-      name: '@nuxtjs/eslint-module',
-      description: 'ESLint module for Nuxt.js',
-      websiteUrl: 'https://github.com/nuxt-modules/eslint'
-    },
-    {
-      id: 'nuxt-js-google-fonts',
-      name: '@nuxtjs/google-fonts',
-      description: 'Google Fonts module for NuxtJS',
-      websiteUrl: 'https://github.com/nuxt-modules/google-fonts'
-    },
-    {
-      id: 'nuxt-pwa',
-      name: '@nuxtjs/pwa',
-      description: 'Zero config PWA solution for Nuxt.js',
-      websiteUrl: 'https://github.com/nuxt-community/pwa-module'
-    }
-  ]
-}
+const all: ProjectItemTypes[] = [
+  ...showcase,
+  {
+    id: 'how-to-become-microsoft-certified-azure-ai-engineer-associate',
+    name: 'Become Microsoft Certified: Azure AI Engineer Associate',
+    websiteUrl: 'https://github.com/damisparks/microsoft-azure-ai-engineer-associate',
+    description: `Resources I used for pursuing certification in Azure AI Engineer Associate.`,
+    imageUrl: '/projects/microsoft-ai-associate-cert.png'
+  },
+  {
+    id: 'nuxt-validate',
+    name: 'Nuxt-validate',
+    websiteUrl: 'https://www.npmjs.com/package/nuxt-validate',
+    description: 'A Simple Nuxt input validation module with the vee-validate library',
+    imageUrl: '/projects/nuxt-validate.png'
+  },
+  {
+    id: 'nuxt-js-eslint-module',
+    name: '@nuxtjs/eslint-module',
+    description: 'ESLint module for Nuxt.js',
+    websiteUrl: 'https://github.com/nuxt-modules/eslint',
+    imageUrl: '/projects/nuxt-eslint.png'
+  },
+  {
+    id: 'nuxt-js-google-fonts',
+    name: '@nuxtjs/google-fonts',
+    description: 'Google Fonts module for NuxtJS',
+    websiteUrl: 'https://github.com/nuxt-modules/google-fonts',
+    imageUrl: '/projects/nuxt-google-fonts.png'
+  },
+  {
+    id: 'nuxt-pwa',
+    name: '@nuxtjs/pwa',
+    description: 'Zero config PWA solution for Nuxt.js',
+    websiteUrl: 'https://github.com/nuxt-community/pwa-module',
+    imageUrl: '/projects/nuxt-pwa.png'
+  }
+]
 
 export { showcase, all }
--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { all } from '@/data/projects'
 
-const title = ref('Projects')
-const description = ref('Things that I created or collaborated on trying to put my dent in the universe.')
+const title = 'Projects'
+const description = 'Things that I created or collaborated on trying to put my dent in the universe.'
 useSeoMeta({
   title,
   description,
@@ -12,8 +12,8 @@ useSeoMeta({
 </script>
 
 <template>
-  <div class="space-y-12 py-8">
-    <section class=" text-center">
+  <div class="space-y-12 py-12">
+    <section class="text-center">
       <AppTypography
         tag="h1"
         class="text-3xl font-medium tracking-tight sm:text-4xl"
@@ -29,35 +29,45 @@ useSeoMeta({
       </AppTypography>
     </section>
     <section class="max-w-5xl mx-auto">
-      <div
-        v-for="(key, keyIdx) in Object.keys(all)"
-        :key="key"
-        :style="{ '--enter-stage': keyIdx + 1 }"
-        slide-enter
-        class="space-y-8"
+      <ul
+        role="list"
+        class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8"
       >
-        <div class="grid grid-cols-1 gap-6 sm:grid-cols-2">
-          <NuxtLink
-            v-for="item in all[key]"
-            :key="item.name"
-            class="p-4 rounded-lg hover:bg-primary-50 dark:hover:bg-primary-500/40 col-span-1"
-            :to="item.websiteUrl"
-            target="_blank"
-            external
+        <li
+          v-for="(item, idx) in all"
+          :key="item.name"
+          :style="{ '--enter-stage': idx + 1 }"
+          slide-enter
+          class="relative shadow-lg rounded-lg p-2"
+        >
+          <div class="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-zinc-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-zinc-100">
+            <img
+              :src="item.imageUrl"
+              alt=""
+              class="pointer-events-none object-cover group-hover:opacity-75"
+            >
+            <button
+              type="button"
+              class="absolute inset-0 focus:outline-none"
+            >
+              <span class="sr-only">View details for {{ item.name }}</span>
+            </button>
+          </div>
+          <AppTypography
+            paragraph
+            class="pointer-events-none mt-2 block truncate text-sm font-medium"
           >
-            <div
-              class="font-montserrat font-medium leading-7 lg:leading-8 lg:text-xl"
-            >
-              {{ item.name }}
-            </div>
-            <div
-              class="text-sm font-normal leading-6 text-zinc-400 dark:text-zinc-400"
-            >
-              {{ item.description }}
-            </div>
-          </NuxtLink>
-        </div>
-      </div>
+            {{ item.name }}
+          </AppTypography>
+          <AppTypography
+            paragraph
+            variant="muted"
+            class="pointer-events-none block text-sm font-medium"
+          >
+            {{ item.description }}
+          </AppTypography>
+        </li>
+      </ul>
     </section>
   </div>
 </template>
