2a290226c2326729696a5a5e1588f8a66d71d9a3
Dami Sparks <damisparks@outlook.com>
2024-10-15 23:21:18 +0200

    update theming
    * update `app.config.ts` for theming
    * remove component and style that duplicates `@nuxt/ui` components
    * update error page

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,6 +1,13 @@
 export default defineAppConfig({
-	ui: {
-		primary: 'royal-blue',
-		gray: 'cool'
-	}
+  ui: {
+    primary: 'royal-blue',
+    gray: 'cool',
+    button: {
+      color: {
+        primary: {
+          solid: 'bg-primary-500 text-white hover:bg-primary-600 dark:hover:bg-primary-500/40 font-normal'
+        }
+      }
+    }
+  }
 })
