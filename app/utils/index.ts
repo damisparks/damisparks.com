de440a2ac20f50d78469300bc0f0be89fa053652
Dami Sparks <damisparks@outlook.com>
2025-04-19 07:15:20 +0200

    chore: remove sitemap generation and bump up `nuxt-og-image`

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,5 +1,6 @@
 /**
  * Format a date based on the locale
+ *
  * @param dt Date object
  * @param locale Locale string
  * @returns Returns a formatted date string based on the locale
--- content.config.ts
+++ content.config.ts
@@ -1,7 +1,14 @@
 import { defineCollection, defineContentConfig, z } from '@nuxt/content'
+import { asOgImageCollection } from 'nuxt-og-image/content'
 
 export default defineContentConfig({
   collections: {
+    content: defineCollection(
+      asOgImageCollection({
+        type: 'page',
+        source: '**/*.md',
+      })
+    ),
     page: defineCollection({
       source: '*.md',
       type: 'page',
