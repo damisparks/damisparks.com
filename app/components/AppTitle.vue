3ade9ca5e24487c02c7152a05054b5429ef36818
Dami Sparks <damisparks@outlook.com>
2025-06-10 22:20:27 +0200

    feat: add `AppTitle` component
    * Add `AppTitle` component for reusable title rendering
     * Update index and projects pages

---
+++ app/components/AppTitle.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{ label: string }>()
+</script>
+
+<template>
+  <span class="text-royalblue-800 dark:text-royalblue-400">
+    {{ props.label }}
+  </span>
+</template>
