43a64192419c3c5e22c43d68c5184a0f31b6b47e
Dami Sparks <damisparks@outlook.com>
2025-06-11 19:49:05 +0200

    feat: enhance NoteCard component with slot support
    * add NoteTitle component; update date formatting logic

--- app/components/NoteCard.vue
+++ app/components/NoteCard.vue
@@ -1,31 +1,10 @@
-<script setup lang="ts">
-import type { NoteTag } from '@/types'
-
-defineProps<{ title: string, description: string, label: NoteTag, to: string }>()
-</script>
-
 <template>
   <div class="flex">
     <div class="mr-4 shrink-0">
       <slot name="media" />
     </div>
-    <div>
-      <NoteBadge :label="label" class="mb-2" />
-      <NuxtLink :to="to">
-        <AppTypography
-          tag="h4"
-          class="font-medium text-lg hover:underline"
-        >
-          {{ title }}
-        </AppTypography>
-      </NuxtLink>
-      <AppTypography
-        variant="muted"
-        paragraph
-        class="mt-1"
-      >
-        {{ description }}
-      </AppTypography>
+    <div v-if="$slots.content">
+      <slot name="content" />
     </div>
   </div>
 </template>
--- app/utils/format-date-by-locale.ts
+++ app/utils/format-date-by-locale.ts
@@ -4,9 +4,14 @@
  * @param dt Date object
  * @param locale Locale string
  * @returns Returns a formatted date string based on the locale
+ *
+ * @example
+ * ```ts
+ * formatDateByLocale(new Date(), 'en-US') // 'June 11, 2025'
+ * ```
  */
-export default function formatDateByLocale (dt: Date, locale: string) {
-  return new Date(dt).toLocaleDateString(locale, {
+export default function formatDateByLocale (dt: Date | string, locale?: string) {
+  return new Date(dt).toLocaleDateString(locale || 'en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
