a3d03338210147e04df111ccc28257456c07cf47
Dami Sparks <damisparks@outlook.com>
2025-06-11 17:31:37 +0200

    feat: add NoteCard, NoteBadge, and NoteAvatar components; update notes page layout and styling

---
+++ app/components/NoteBadge.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { BadgeProps } from '@nuxt/ui'
+import type { NoteTag } from '@/types'
+
+type NoteTagColor = BadgeProps['color']
+
+const props = defineProps<{ label: NoteTag }>()
+
+const setNoteTagColor = (label: NoteTag): NoteTagColor => {
+  switch (label) {
+    case 'cloud':
+      return 'info'
+    case 'typescript':
+      return 'secondary'
+    case 'vue':
+      return 'success'
+    case 'nuxt':
+      return 'secondary'
+    case 'personal':
+      return 'primary'
+    case 'books':
+      return 'error'
+  }
+}
+</script>
+
+<template>
+  <UBadge
+    :color="setNoteTagColor(props.label)"
+    :label="props.label"
+    variant="soft"
+    class="uppercase font-semibold"
+  />
+</template>
---
+++ app/components/NoteCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { NoteTag } from '@/types'
+
+defineProps<{ title: string, description: string, label: NoteTag, to: string }>()
+</script>
+
+<template>
+  <div class="flex">
+    <div class="mr-4 shrink-0">
+      <slot name="media" />
+    </div>
+    <div>
+      <NoteBadge :label="label" class="mb-2" />
+      <NuxtLink :to="to">
+        <AppTypography
+          tag="h4"
+          class="font-medium text-lg hover:underline"
+        >
+          {{ title }}
+        </AppTypography>
+      </NuxtLink>
+      <AppTypography
+        variant="muted"
+        paragraph
+        class="mt-1"
+      >
+        {{ description }}
+      </AppTypography>
+    </div>
+  </div>
+</template>
