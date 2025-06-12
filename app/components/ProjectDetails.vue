d126acb16bfa9d9b77a53b9adce7d4a82868c727
Dami Sparks <damisparks@outlook.com>
2025-06-12 21:03:41 +0200

    feat: add ProjectDetails and ProjectImage components
    * update projects page layout and functionality

---
+++ app/components/ProjectDetails.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ label: string, description: string }>()
+</script>
+
+<template>
+  <div class="mt-2">
+    <AppTypography class="pointer-events-none block truncate text-sm font-medium">
+      {{ label }}
+    </AppTypography>
+    <AppTypography class="pointer-events-none block text-sm font-medium" variant="muted">
+      {{ description }}
+    </AppTypography>
+  </div>
+</template>
---
+++ app/components/ProjectImage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{ src: string, alt: string }>()
+</script>
+
+<template>
+  <div class="overflow-hidden rounded-lg bg-neutral-100">
+    <NuxtImg
+      :src="src"
+      :alt="alt"
+      class="object-cover aspect-10/7 transform-3d hover:scale-105 hover:translate-z-0 transition-transform duration-600"
+      width="512"
+      height="512"
+      loading="lazy"
+    />
+  </div>
+</template>
